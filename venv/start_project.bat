14c68d9a1ea7d87796c6388cc0a018d55a693049
MehulDP2010 <160006294+MehulDP2010@users.noreply.github.com>
2025-03-08 10:45:02 +0530

    Fixed project structure

---
+++ venv/start_project.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d C:\Users\User\OneDrive\Desktop\MyAlgoTradingProject\morningstararise
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoExit -Command "& 'venv\Scripts\Activate.ps1'"
